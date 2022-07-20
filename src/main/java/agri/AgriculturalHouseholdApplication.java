e47d6a89984f469e194804ebb8ce8af7c40e5d68
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-07-20 17:01:03 +0200

    Initial commit

---
+++ src/main/java/agri/AgriculturalHouseholdApplication.java
@@ -0,0 +1,13 @@
+package agri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgriculturalHouseholdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgriculturalHouseholdApplication.class, args);
+	}
+
+}
