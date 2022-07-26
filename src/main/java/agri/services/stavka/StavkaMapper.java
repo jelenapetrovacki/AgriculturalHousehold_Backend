97bec9c4a9041d1a70b6c36bf49aeff854737c1a
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-07-26 15:34:25 +0200

    Popravljen naziv tipa proizvoda za stavku

--- src/main/java/agri/services/stavka/StavkaMapper.java
+++ src/main/java/agri/services/stavka/StavkaMapper.java
@@ -31,7 +31,6 @@ public interface StavkaMapper {
             @Mapping(target = "tipProizvoda.nazivTipa", source="api.tip_proizvoda.naziv_tipa"),
             @Mapping(target = "tipProizvoda.jedinicnaCena", source="api.tip_proizvoda.jedinicna_cena")
     })
-    //moraju se mapirati obelezja sva
     Stavka apiToEntity(StavkaModel api);
 
 
