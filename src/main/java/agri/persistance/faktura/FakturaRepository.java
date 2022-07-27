77d0203fab773ef9943b92a5bff25b3e43422d9e
jelenapetrovacki <jpetrovacki3@gmail.com>
2022-07-27 13:37:15 +0200

    a

--- src/main/java/agri/persistance/faktura/FakturaRepository.java
+++ src/main/java/agri/persistance/faktura/FakturaRepository.java
@@ -8,7 +8,7 @@ import java.util.Collection;
 public interface FakturaRepository extends JpaRepository<Faktura,Integer> {
     Collection<Faktura> findByNarudzbinaId(int narudzbinaId);
     
-    @Query(value = "select count(*)"
+    @Query(value = "select count(*) "
     		+ "from faktura join uplata on (faktura.id=uplata.faktura) where faktura.id = ?1", nativeQuery = true)
 	Integer brojUplata(Integer fakturaId);
 }
